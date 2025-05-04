9ae78366436e6d90ea7feb3e022a40069552f1c4
Jonathan Schad <mail@jschad.de>
2025-05-04 09:23:40 +0200

    Fix beackend eslint

--- packages/eslint-config/nestjs.config.mjs
+++ packages/eslint-config/nestjs.config.mjs
@@ -10,29 +10,5 @@ export default tseslint.config(...baseConfig, {
                 patterns: ['.*'],
             },
         ],
-        'simple-import-sort/imports': [
-            'error',
-            {
-                groups: [
-                    // 1. Node.js builtins and external packages from node_modules
-                    ['^(node:)?\\w', '^@(?!server|client|boilerplate)\\w'],
-
-                    // 2. Local imports (from the current app/package)
-                    ['^@client'],
-
-                    // 3. Imports from other apps/packages in your monorepo
-                    ['^@server', '^@boilerplate'],
-
-                    // 4. Side effect imports
-                    ['^\\u0000'],
-
-                    // 5. Parent imports. Put `..` last.
-                    ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-
-                    // 6. Other relative imports. Put same-folder imports and `.` last.
-                    ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-                ],
-            },
-        ],
     },
 });
