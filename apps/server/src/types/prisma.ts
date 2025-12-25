941f01fb16e63674a40b55f1a43c53ea7bc82631
Jonathan Schad <mail@jschad.de>
2025-12-25 20:57:15 +0100

    Fix typescript errors

--- apps/server/src/types/prisma.ts
+++ apps/server/src/types/prisma.ts
@@ -1,5 +1,3 @@
-import type { Game, GameVisit, User, UserSettings } from '@boilerplate/prisma';
+import type { User, UserSettings } from '@boilerplate/prisma';
 
 export type UserWithSettings = User & { settings: UserSettings };
-
-export type GameWithVisits = Game & { visits: GameVisit[] };
